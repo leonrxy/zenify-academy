e869dc68f9659eb2693bf331ec5971da413fb354
Leonardus Reka Jakti <leonardusrekajaktipuspito15@gmail.com>
2023-12-19 10:41:38 +0700

    reka

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ApiLoginController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', [ApiLoginController::class, 'Login']);
+Route::post('/login', [LoginController::class, 'apiLogin']);
